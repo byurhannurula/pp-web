9138af9744959abf0da81cb831c8b7f25c681bc4
Byurhan Beyzat <imbyurhan@gmail.com>
2019-06-25 18:48:15 +0300

    add config file

---
+++ config.js
@@ -0,0 +1,9 @@
+export default {
+  dev: {
+    SERVER_URL: `http://localhost:4000`,
+  },
+  prod: {
+    SERVER_URL: `http://api.pokerplanning.me`,
+    SERVER_QUERY_URL: `http://api.pokerplanning.me/graphql`,
+  },
+}
